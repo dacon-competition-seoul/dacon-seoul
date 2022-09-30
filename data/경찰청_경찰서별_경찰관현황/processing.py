ea551fea46ea9073d5b3d99c1ca5a8187801f04a
howsmyanimeprofilepicture <howsmyanimeprofilepicture@gmail.com>
2022-09-30 15:16:24 +0900

    feat: 서울내 경찰서별 정원 데이터 전처리

---
+++ data/경찰청_경찰서별_경찰관현황/processing.py
@@ -0,0 +1,39 @@
+import pandas as pd
+from pathlib import Path
+
+
+def main():
+    __dirname = Path(__file__).parent
+    df = pd.concat(
+        [
+            pd.read_csv(
+                __dirname.joinpath("경찰청_전국 경찰서별 경찰관 현황_12_31_2015.csv"),
+                encoding="cp949",
+            ).rename(columns={" 경찰관 ": "경찰관"}),
+            pd.read_csv(
+                __dirname.joinpath("경찰청_경찰서별정원_20211014.csv"), encoding="cp949"
+            ),
+        ]
+    )
+
+    df = df[df["지방청"] == "서울"]
+    df = df.rename(columns={"년도": "ds", "경찰서": "office", "경찰관": "quota"}).drop(
+        "지방청", axis=1
+    )
+    answer = []
+    for num in df.quota.to_list():
+        try:
+            num = int(num)
+        except:
+            num = num.replace(",", "")
+            num = int(num)
+        answer.append(num)
+
+    df["quota"] = answer
+    df["ds"] = df.ds.str[:4]
+    df["office"] = df.office.str[2:]
+    df.to_csv(__dirname.joinpath("2014-2021_서울_경찰서별_정원.csv"), index=False)
+
+
+if __name__ == "__main__":
+    main()
